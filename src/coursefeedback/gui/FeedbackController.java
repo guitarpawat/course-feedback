9eddb3dffb771bbccfc72e1a27e9eaa0205bc41d
Noppawan Kulchol <noppawankulchol@hotmail.com>
2017-05-21 18:14:01 +0700

    changed font and added feedback's UI

---
+++ src/coursefeedback/gui/FeedbackController.java
@@ -0,0 +1,123 @@
+package coursefeedback.gui;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXRadioButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
+
+public class FeedbackController {
+
+	@FXML
+	private ToggleGroup q1;
+
+	@FXML
+	private ToggleGroup q2;
+
+	@FXML
+	private ToggleGroup q3;
+
+	@FXML
+	private ToggleGroup q4;
+
+	@FXML
+	private ToggleGroup q5;
+	
+	@FXML
+	private JFXRadioButton q10;
+
+	@FXML
+	private JFXRadioButton q55;
+
+	@FXML
+	private JFXRadioButton q45;
+
+	@FXML
+	private JFXRadioButton q44;
+
+	@FXML
+	private JFXRadioButton q43;
+
+	@FXML
+	private JFXRadioButton q35;
+
+	@FXML
+	private JFXRadioButton q34;
+
+	@FXML
+	private JFXRadioButton q33;
+
+	@FXML
+	private JFXRadioButton q25;
+
+	@FXML
+	private JFXRadioButton q24;
+
+	@FXML
+	private JFXRadioButton q23;
+
+	@FXML
+	private JFXRadioButton q12;
+
+	@FXML
+	private JFXRadioButton q11;
+
+	@FXML
+	private JFXRadioButton q15;
+
+	@FXML
+	private JFXRadioButton q14;
+
+	@FXML
+	private JFXRadioButton q13;
+
+	@FXML
+	private JFXRadioButton q42;
+
+	@FXML
+	private JFXRadioButton q32;
+
+	@FXML
+	private JFXRadioButton q22;
+
+	@FXML
+	private JFXRadioButton q41;
+
+	@FXML
+	private JFXRadioButton q31;
+
+	@FXML
+	private JFXRadioButton q21;
+
+	@FXML
+	private JFXRadioButton q40;
+
+	@FXML
+	private JFXRadioButton q30;
+
+	@FXML
+	private JFXRadioButton q54;
+
+	@FXML
+	private JFXRadioButton q53;
+
+	@FXML
+	private JFXRadioButton q52;
+
+	@FXML
+	private JFXRadioButton q51;
+
+	@FXML
+	private JFXRadioButton q50;
+
+	@FXML
+	private JFXRadioButton q20;
+
+	@FXML
+	private JFXButton sendButton;
+	
+	public void sendFeedback(ActionEvent event){
+		
+	}
+}
