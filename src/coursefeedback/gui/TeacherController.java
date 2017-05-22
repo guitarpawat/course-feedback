331c8b34d2aaa915e5563e59131620dea2e79eca
Noppawan Kulchol <noppawankulchol@hotmail.com>
2017-05-22 21:44:24 +0700

    added Result

--- src/coursefeedback/gui/TeacherController.java
+++ src/coursefeedback/gui/TeacherController.java
@@ -1,5 +1,137 @@
 package coursefeedback.gui;
 
+import java.awt.Label;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.fxml.FXML;
+
 public class TeacherController {
+	
+	@FXML
+    private Label welcomTeacher;
+
+    @FXML
+    private Label nameTeacher;
+
+    @FXML
+    private Label IDTeach1;
+
+    @FXML
+    private Label IDTeach2;
+
+    @FXML
+    private Label IDTeach3;
+
+    @FXML
+    private Label IDTeach4;
+
+    @FXML
+    private Label IDTeach5;
+
+    @FXML
+    private Label IDTeach6;
+
+    @FXML
+    private Label IDTeach7;
+
+    @FXML
+    private Label IDTeach8;
+
+    @FXML
+    private Label IDTeach9;
+
+    @FXML
+    private Label IDTeach10;
+
+    @FXML
+    private Label courseTeach1;
+
+    @FXML
+    private Label courseTeach10;
+
+    @FXML
+    private Label courseTeach9;
+
+    @FXML
+    private Label courseTeach8;
+
+    @FXML
+    private Label courseTeach7;
+
+    @FXML
+    private Label courseTeach6;
+
+    @FXML
+    private Label courseTeach5;
+
+    @FXML
+    private Label courseTeach4;
+
+    @FXML
+    private Label courseTeach3;
+
+    @FXML
+    private Label courseTeach2;
+
+    @FXML
+    private Label secTeach1;
+
+    @FXML
+    private Label secTeach10;
+
+    @FXML
+    private Label secTeach9;
+
+    @FXML
+    private Label secTeach8;
+
+    @FXML
+    private Label secTeach7;
+
+    @FXML
+    private Label secTeach6;
+
+    @FXML
+    private Label secTeach5;
+
+    @FXML
+    private Label secTeach4;
+
+    @FXML
+    private Label secTeach3;
+
+    @FXML
+    private Label secTeach2;
+
+    @FXML
+    private JFXButton feedTeach1;
+
+    @FXML
+    private JFXButton feedTeach10;
+
+    @FXML
+    private JFXButton feedTeach9;
+
+    @FXML
+    private JFXButton feedTeach8;
+
+    @FXML
+    private JFXButton feedTeach7;
+
+    @FXML
+    private JFXButton feedTeach6;
+
+    @FXML
+    private JFXButton feedTeach5;
+
+    @FXML
+    private JFXButton feedTeach4;
+
+    @FXML
+    private JFXButton feedTeach3;
+
+    @FXML
+    private JFXButton feedTeach2;
 
 }
